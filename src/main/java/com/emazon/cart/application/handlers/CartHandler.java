7981d9c3e77f747467e1e2e0007344688a522c62
Juan Gerardo <jmendez1025@cue.edu.co>
2024-11-23 21:19:09 -0500

    feat: connection with stripe and first checkout implemented

--- src/main/java/com/emazon/cart/application/handlers/CartHandler.java
+++ src/main/java/com/emazon/cart/application/handlers/CartHandler.java
@@ -2,13 +2,16 @@ package com.emazon.cart.application.handlers;
 
 import com.emazon.cart.application.dtos.cart.AddArticleToCartRequestDTO;
 import com.emazon.cart.application.dtos.cart.ArticlesCartResponseDTO;
+import com.emazon.cart.application.dtos.cart.CartResponseDTO;
 import com.emazon.cart.application.dtos.common.ResponseDTO;
 import com.emazon.cart.application.dtos.pagination.PaginationRequestDTO;
 import com.emazon.cart.application.dtos.searchcriteria.ArticleSearchCriteriaRequestDTO;
 import com.emazon.cart.application.dtos.sorting.SortingRequestDTO;
+import com.emazon.cart.domain.models.CartWithArticles;
 
 public interface CartHandler {
   ResponseDTO addArticleToCart(AddArticleToCartRequestDTO addArticleToCartRequestDTO);
   ResponseDTO removeArticleFromCart(Long articleId);
   ArticlesCartResponseDTO getCartArticles(SortingRequestDTO sorting, PaginationRequestDTO pagination, ArticleSearchCriteriaRequestDTO searchCriteria);
+  CartWithArticles getUserCart();
 }
